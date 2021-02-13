118a566578ca22663503d2beedd34e9e30ceec46
francinv <francinv@stud.ntnu.no>
2021-02-14 00:47:01 +0100

    Created tests for checking if correct url is opened

--- APP/tests/test_urls.py
+++ APP/tests/test_urls.py
@@ -1,2 +1,11 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from APP.views import home
+
 
 class TestUrls(SimpleTestCase):
+
+    def test_APP_home_url_resolve(self):
+        url = reverse('APP-home')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, home)
