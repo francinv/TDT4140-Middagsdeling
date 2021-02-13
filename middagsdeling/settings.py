02bd8f134d983dcbe514adc03fd3f2b3bff65be3
francinv <francinv@stud.ntnu.no>
2021-02-13 21:33:29 +0100

    Added appconfigs and crispy_forms, related #2

--- middagsdeling/settings.py
+++ middagsdeling/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
+    'APP.apps.AppConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
