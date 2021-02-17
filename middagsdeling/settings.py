4e43fd29a543df4ea7a6350f3a5546525940f8a8
Haestad <54801050+Haestad@users.noreply.github.com>
2021-02-17 15:46:35 +0100

    updated db settings

--- middagsdeling/settings.py
+++ middagsdeling/settings.py
@@ -86,18 +86,17 @@ WSGI_APPLICATION = 'middagsdeling.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
-DATABASES = {'default':{
-    'ENGINE' : 'django.db.backends.postgresql_psycopg2',
-    'NAME' : 'middagsdeling',
-    'USER' : 'postgres',
-    'PASSWORD' : 'admin',
-    'HOST' : 'localhost',
-    'PORT' : '5432'
+DATABASES = {'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'postgres',
+    'USER': 'postgres',
+    'PASSWORD': 'data1',
+    'HOST': 'localhost',
+    'PORT': '5432'
     }
 }
-DATABASES['default'].update(db_from_env)
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
