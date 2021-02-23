460d993cde0b5bb5ae5b04c0b1e302d224e4114b
mikkeldah <mikkelds@stud.ntnu.no>
2021-02-23 13:04:18 +0100

    completed create-post.html/css and created create-post.js

---
+++ static/js/create-post.js
@@ -0,0 +1,9 @@
+function showDinnerCost() {
+    var dinnerCostItems = document.getElementById('amount-container');
+    dinnerCostItems.style.display = "block";
+}
+
+function hideDinnerCost() {
+    var dinnerCostItems = document.getElementById('amount-container');
+    dinnerCostItems.style.display = "none";
+}
