b08f0f7ee516fc59124cb93c63462234738de8c2
francinv <francinv@stud.ntnu.no>
2021-02-23 13:03:06 +0100

    Changed model and added multiselectfield

--- APP/models.py
+++ APP/models.py
@@ -2,39 +2,33 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
+from multiselectfield import MultiSelectField
+
+sharing_choices = [
+        ('ja', 'Ja'),
+        ('no', 'Nei'),
+]
+allergener_choices =[
+        ('GLUTEN', 'Gluten'),
+        ('LAKTOSE', 'Laktose'),
+        ('EGG', 'Egg'),
+        ('NØTTER', 'Nøtter'),
+]
 
 class Middag(models.Model):
     title = models.CharField(max_length=125)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    guests = models.IntegerField()
-    YES = 'Y'
-    NO = 'N'
-    GLUTEN = 'GL'
-    LAKTOSE = 'LA'
-    EGG = 'EG'
-    NØTTER = 'NØ'
-    sharing_choices = [
-        (YES, 'Ja'),
-        (NO, 'Nei'),
-    ]
+    guests = models.PositiveSmallIntegerField()
     sharing = models.CharField(
         max_length=2,
         choices=sharing_choices,
-        default=YES,
+        default='ja',
     )
-    allergener_choices =[
-        (GLUTEN, 'Gluten'),
-        (LAKTOSE, 'Laktose'),
-        (EGG, 'Egg'),
-        (NØTTER, 'Nøtter'),
-    ]
-    allergener = models.CharField(
-        max_length=2,
-        choices=allergener_choices,
-        default=None
-    )
+    sharing_number = models.PositiveSmallIntegerField()
+    allergener = MultiSelectField(choices=allergener_choices)
+
 
     def __str__(self):
         return self.title
