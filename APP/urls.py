2c6d9debddbc367227c3bef582fd993ce62ab53c
francinv <francinv@stud.ntnu.no>
2021-02-23 13:03:25 +0100

    Minor changes

--- APP/urls.py
+++ APP/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+
 from .views import (
     MiddagListView,
     MiddagDeleteView,
     MiddagUpdateView,
     MiddagCreateView,
-    MiddagDetailView
+    MiddagDetailView,
 )
 
 urlpatterns = [
