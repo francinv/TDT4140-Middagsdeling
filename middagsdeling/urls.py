bc269fd5a9eb6bce49b75ad1c6186edb60a50925
francinv <francinv@stud.ntnu.no>
2021-02-23 13:03:30 +0100

    Minor changes

--- middagsdeling/urls.py
+++ middagsdeling/urls.py
@@ -19,11 +19,10 @@ from users import views as user_views
 from django.contrib.auth import views as auth_views
 
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', user_views.register, name='register'),
     path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('home/', include('APP.urls')),
 ]
