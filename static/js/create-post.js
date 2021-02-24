0347fdbcc0aa170a0b746cec6e584f55a2d39e05
francinv <francinv@stud.ntnu.no>
2021-02-24 13:23:05 +0100

    Added function placeholder

--- static/js/create-post.js
+++ static/js/create-post.js
@@ -7,3 +7,12 @@ function hideDinnerCost() {
     var dinnerCostItems = document.getElementById('amount-container');
     dinnerCostItems.style.display = "none";
 }
+
+function addPlaceholder(){
+    var address = document.getElementById('id_address');
+    var postnr = document.getElementById('id_postnr');
+    var poststed = document.getElementById('id_poststed');
+    address.placeholder = "Eksempelveien 1";
+}
+
+addPlaceholder()
