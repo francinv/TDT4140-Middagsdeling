3e4f3e566a2902bfc478bdc98665e05b756d98a5
francinv <francinv@stud.ntnu.no>
2021-02-24 21:14:17 +0100

    calculate_cost.js for calculating amount for customer, not working yet.

--- static/js/calculate_cost.js
+++ static/js/calculate_cost.js
@@ -1,7 +1,7 @@
 function calculate(){
-    let num1 = document.getElementById('middag_sharing_cost');
-    let num2 = document.getElementById('middag_guests');
-    const num3 = num1 / num2;
+    num1 = document.getElementById('middag_sharing_cost');
+    num2 = document.getElementById('middag_guests');
+    num3 = num1 / num2;
     console.log("HEI");
     console.log(num3);
     document.getElementById("sharing_cost_number").innerHTML = num3;
